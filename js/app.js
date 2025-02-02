350994dbd9d19abbdca459437eebdb22f5275e9d
Bradley <bradleyranaka@gmail.com>
2025-02-02 15:20:35 +0200

    Basic calculator functions complete

--- js/app.js
+++ js/app.js
@@ -21,7 +21,38 @@ const handleButtonClicks = (event) => {
     }
 };
 
+const evaluateExpression = (expression) => {
+    const tokens = expression.match(/(\d+\.?\d*)|([+\-*/])/g);
+    if (!tokens) throw new Error("uhh");
+    let result = parseFloat(tokens[0]);
+
+    for (let i = 1; i <= tokens.length; i += 2) {
+        const operator = tokens[i];
+        const nextNum = parseFloat(tokens[i + 1]);
+
+        switch (operator) {
+            case "+":
+                result += nextNum;
+                break;
+            case "-":
+                result -= nextNum;
+                break;
+            case "/":
+                if (nextNum === 0) throw new Error("can't do that ;)");
+                result /= nextNum;
+                break;
+            case "*":
+                result *= nextNum;
+                break;
+        }
+    }
+
+    return result;
+};
+
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", handleButtonClicks);
 });
+
+console.log(evaluateExpression(3+5));
